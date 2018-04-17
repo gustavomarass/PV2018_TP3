a3dc5b0988bb1103eab297117915b2f7021b6ecb
gustavo <gustavo@gustavo-PC>
2018-04-16 23:30:48 -0300

    modificacion punto 1 y 2

--- src/Punto_02/test/TestRectangulo.java
+++ src/Punto_02/test/TestRectangulo.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
  */
 package Punto_02.test;
 
@@ -15,16 +13,13 @@ import java.util.Scanner;
  */
 public class TestRectangulo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         Scanner teclado = new Scanner(System.in);
         Rectangulo miRectanulo = new Rectangulo();
         Punto miPunto = new Punto();
 
-        System.out.println("----- CALCULAR POSICION-----");
+        System.out.println("02_ Calcular puntos restantes Rectangulo");
         System.out.println("P1(x1,y1)-> ingrese x1: ");
         double x1 = teclado.nextDouble();
         System.out.println("P1(x1,y1)-> ingrese y1: ");
@@ -45,8 +40,8 @@ public class TestRectangulo {
         System.out.println("P3(x3,y3)= "+"P3("+miRectanulo.getPuntoTres().getX()+","+miRectanulo.getPuntoTres().getY()+")");
         miRectanulo.calcularPto4();
         System.out.println("P4(x4,y4)= "+"P4("+miRectanulo.getPuntoCuatro().getX()+","+miRectanulo.getPuntoCuatro().getY()+")");
-        System.out.println("Superficie: "+miRectanulo.calcularSuperficie());
-        System.out.println("Perimetro: "+miRectanulo.calcularPerimetro());
+        System.out.println("Superficie= "+miRectanulo.calcularSuperficie());
+        System.out.println("Perimetro= "+miRectanulo.calcularPerimetro());
     }
     
 }
